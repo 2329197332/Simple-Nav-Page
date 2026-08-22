c8f70bc5507902be4e3716260093386333853616
稀饭 <70185840+2329197332@users.noreply.github.com>
2026-08-22 23:02:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ── 图标 & 背景 配置 ────────────────────────────────────────
 const FAVICON_PROVIDER = 'google';
-const PROXY = '';
+const PROXY = 'https://nav.2329197332.workers.dev';
 
 function withProxy(originUrl) {
   if (!PROXY) return originUrl;
